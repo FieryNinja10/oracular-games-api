406420bf3c5cc39eb8b5341b593fd3cde17ed73e
FieryNinja10 <zarar_ahmed@outlook.com>
2024-03-23 18:38:40 -0400

    chore: unnecessary env variable

--- src/env.ts
+++ src/env.ts
@@ -4,8 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     PG_URL: z.string(),
-    NEXTAUTH_URL: z.string().url(),
-    NEXTAUTH_SECRET: z.string(),
+    CLIENT_URL: z.string().url(),
     JWT_SECRET: z.string(),
   },
   runtimeEnv: process.env,
